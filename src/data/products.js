df53a4d876118b904d3e50be25f1601f21274257
SREYA RAJESH <sreyarajesh110805@gmail.com>
2025-07-27 22:22:21 +0530

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,4 +1,6 @@
-import image1 form '../assets/image1.png';
+import image1 from '../assets/image1.png';
+import image2 from '../assets/image2.png';
+import image3 from '../assets/image3.png';
 const products = [
   {
     id: 1,
